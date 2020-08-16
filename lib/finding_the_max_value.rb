47725806a10b5be71d44f73327ea24d06806587d
Tej Charfi <charfi@usc.edu>
2020-08-16 17:39:20 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,14 @@
 def find_max_value(array)
-  # Add your solution here
+
+  ctr = 0 
+  
+  while ctr < array.length do
+    max = 0 
+    
+    if(array[ctr] > max)
+        max= array[ctr]
+    end
+    
+    ctr+=1
+  end
 end
